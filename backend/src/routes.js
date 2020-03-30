552161ecae54342a04100c41ea19742099295b33
Thiago <tdias@cloudata.cl>
2020-03-29 23:44:51 -0300

    backend tests

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
-
+const { celebrate, Segments, Joi } = require('celebrate');
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
@@ -16,12 +16,36 @@ routes.get('/', (request, response) => {
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().min(2).max(4),
+    })
+}), OngController.create);
 
-routes.get('/incidents', IncidentController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object().keys({
+        authorization: Joi.string().required(),            
+    }).options({ allowUnknown: true }),
+}), ProfileController.index);
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    }),
+}), IncidentController.index);
+
+//TODO VALIDATE CREATION + AUTHORIZATION
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 module.exports = routes;
